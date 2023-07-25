de4005fb46b4d7e7b3ce64dd397376fa407975f9
MrSterdy <mrsterdy@gmail.com>
2023-07-25 14:03:30 +0400

    Add marketplace

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import Leaderboards from "./leaderboards";
 import Players from "./players";
+import Marketplace from "./marketplace";
 
 export * from "./leaderboards";
 export * from "./players";
@@ -9,6 +10,7 @@ export * from "./players";
 export default class API {
     public readonly leaderboards: Leaderboards;
     public readonly players: Players;
+    public readonly marketplace: Marketplace;
 
     public constructor(timeout = 10 * 1000) {
         const axiosInstance = axios.create({
@@ -18,5 +20,6 @@ export default class API {
 
         this.leaderboards = new Leaderboards(axiosInstance);
         this.players = new Players(axiosInstance);
+        this.marketplace = new Marketplace(axiosInstance);
     }
 }
---
+++ src/marketplace/index.ts
@@ -0,0 +1,36 @@
+import Wrapper from "../wrapper";
+
+export type MarketplaceItem = {
+    /** @example 4e6e58a5-2da7-4d3e-9e42-65125f2da2ee */
+    id: string;
+    /** @example goblin-egg-skin */
+    searchName: string;
+    /** @example Goblin - Egg Skin */
+    name: string;
+    /** @example 2022-12-13T22:48:23.463Z */
+    creationDate: string;
+    /** @example This poor little Goblin has been captured and they now occupy your egg. You must sadly keep them locked up in order to win your EggWars game. If your egg is broken, they will escape! */
+    description: string;
+    /** @example https://xforgeassets001.xboxlive.com/pf-namespace-b63a0803d3653643/562a11ee-cfa9-455d-8bce-84d2390d1a9e/GoblinEggSkin_Thumbnail_0.jpg */
+    thumbnail: string;
+    /** @example 490 */
+    price: number;
+    /** @example 4.6 */
+    rating: number;
+    /** @example true */
+    available: boolean;
+};
+
+export default class Marketplace extends Wrapper {
+    public getNames() {
+        return this.instance
+            .get<string[]>("/bedrock/marketplace/names")
+            .then(r => r.data);
+    }
+
+    public getBySearchName(searchName: string) {
+        return this.instance
+            .get<MarketplaceItem>(`/bedrock/marketplace/names/${searchName}`)
+            .then(r => r.data);
+    }
+}
