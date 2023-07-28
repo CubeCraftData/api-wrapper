fa074a155988c4495eb5929b69a4d32247529a5c
MrSterdy <mrsterdy@gmail.com>
2023-07-28 21:26:04 +0400

    Update Xp Wrapper

--- src/xp/index.ts
+++ src/xp/index.ts
@@ -11,4 +11,25 @@ export default class Xp extends Wrapper {
     getAll() {
         return this.instance.get<GameXp[]>("/bedrock/xp").then(r => r.data);
     }
+
+    /**
+     * ONLY FOR INTERNAL USE
+     */
+    updateAll(xps: GameXp[]) {
+        return this.instance
+            .post("/bedrock/xp", xps)
+            .then(() => {});
+    }
+
+    getNames() {
+        return this.instance
+            .get<string[]>("/bedrock/xp/names")
+            .then(r => r.data);
+    }
+
+    getByName(name: string) {
+        return this.instance
+            .get<GameXp>(`/bedrock/xp/name/${name}`)
+            .then(r => r.data);
+    }
 }
